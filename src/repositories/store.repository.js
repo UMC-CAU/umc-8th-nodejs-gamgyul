2175907557f07b0a7e8bdfc7adee6a6ac3ad719a
Hyoin Kang <103008960+hyoinkang@users.noreply.github.com>
2025-05-07 12:49:19 +0900

    [REFACTOR/mission6] ORM을 이용한 repository 리팩토링 (#15)
    * refactor: prisma를 이용한 repositories 리팩토링
    
    - repository를 sql 쿼리 대신 prisma로 구현
    - dto의 response 부분 수정
    - dto의 parameter가 int형으로 처리되도록 명시적으로 수정
    - npm uninstall mysql2
    
    * refactor : 이슈 템플릿 구조 변경

--- src/repositories/store.repository.js
+++ src/repositories/store.repository.js
@@ -1,28 +1,16 @@
-import { pool, prisma } from "../db.config.js";
+import { prisma } from "../db.config.js";
 
 // Review 데이터 삽입
 export const addReview = async (data) => {
-  const conn = await pool.getConnection();
-
-  try {
-    const [result] = await pool.query(
-      `INSERT INTO review (star_point, member_id, store_id, description) VALUES (?, ?, ?, ?);`,
-      [
-        data.starPoint,
-        data.memberId,
-        data.storeId,
-        data.content,
-      ]
-    );
-
-    return result.insertId;
-  } catch (err) {
-    throw new Error(
-      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
-    );
-  } finally {
-    conn.release();
-  }
+  const review = await prisma.review.create({
+    data: {
+      description: data.content,
+      starPoint: data.starPoint,
+      memberId: data.memberId,
+      storeId: data.storeId,
+    },
+  });
+  return review.id;
 };
 
 //getReviewByStore
@@ -45,46 +33,12 @@ export const getAllStoreReviews = async(storeId, cursor) =>{
 
 // 리뷰 정보 얻기
 export const getReview = async (reviewId) => {
-  const conn = await pool.getConnection();
-
-  try {
-    const [review] = await pool.query(`SELECT * FROM review WHERE id = ?;`, reviewId);
-
-    console.log(review);
-
-    if (review.length == 0) {
-      return null;
-    }
-
-    return review;
-  } catch (err) {
-    throw new Error(
-      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
-    );
-  } finally {
-    conn.release();
-  }
+  const review = await prisma.review.findFirstOrThrow({where: {id: reviewId}});
+  return review;
 };
 
 // 가게 정보 얻기
 export const getStore = async (storeId) => {
-  const conn = await pool.getConnection();
-
-  try {
-    const [store] = await pool.query(`SELECT * FROM store WHERE id = ?;`, storeId);
-
-    console.log(store);
-
-    if (store.length == 0) {
-      return null;
-    }
-
-    return store;
-  } catch (err) {
-    throw new Error(
-      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
-    );
-  } finally {
-    conn.release();
-  }
+  const store = await prisma.store.findFirstOrThrow({where: {id: storeId}});
+  return store;
 };
