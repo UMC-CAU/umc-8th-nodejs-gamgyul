40f69c3d25a0ed77f1fe1dd8d4d430546e2319dc
hyoinkang <hyoin1704@gmail.com>
2025-05-28 16:25:08 +0900

    feat : 구글 로그인을 위한 인증 코드 추가
    - client 정보를 env로 관리
    - passport 라이브러리를 이용하여 인증 코드 구현
    - db.config.js 파일의 디렉토리 구조 변경

--- src/repositories/mission.repository.js
+++ src/repositories/mission.repository.js
@@ -1,5 +1,5 @@
 import { MissionStatus } from "@prisma/client";
-import { prisma } from "../db.config.js";
+import { prisma } from "../config/db.config";
 
 // Mission 데이터 삽입
 export const addMission = async (data) => {
--- src/repositories/store.repository.js
+++ src/repositories/store.repository.js
@@ -1,4 +1,4 @@
-import { prisma } from "../db.config.js";
+import { prisma } from "../config/db.config.js";
 
 // Review 데이터 삽입
 export const addReview = async (data) => {
