57455d7fa431c108e5ce48c7e5716ab71fd10868
Hyoin Kang <103008960+hyoinkang@users.noreply.github.com>
2025-06-02 23:38:10 +0900

    [FEAT/week9] 소셜 로그인 기능 구현 (#36)
    * feat : 구글 로그인을 위한 인증 코드 추가
    
    - client 정보를 env로 관리
    - passport 라이브러리를 이용하여 인증 코드 구현
    - db.config.js 파일의 디렉토리 구조 변경
    
    * feat : 소셜 로그인(Google)에 세션 적용
    
    - 세션으로 로그인 정보를 유지
    - prisma와 passport 라이브러리로 편하게 세션을 적용할 수 있음.
    - prisma 스키마에 Session 설정
    - 세션 관리를 위한 미들웨어 설정

--- src/repositories/mission.repository.js
+++ src/repositories/mission.repository.js
@@ -1,5 +1,5 @@
 import { MissionStatus } from "@prisma/client";
-import { prisma } from "../db.config.js";
+import { prisma } from "../config/db.config.js";
 
 // Mission 데이터 삽입
 export const addMission = async (data) => {
--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,4 +1,4 @@
-import { prisma } from "../db.config.js";
+import { prisma } from "../config/db.config.js";
 
 // User 데이터 삽입
 export const addUser = async (data) => { 
